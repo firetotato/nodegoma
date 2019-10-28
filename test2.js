791c4a28d5072f8a86c5ec529c0006a97032e4fa
firetotato <55518203+firetotato@users.noreply.github.com>
2019-10-28 17:24:11 +0900

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var server = http.createServer();
+
+var port = 3000;
+server.listen(port, function(){
+    console.log('웹 서버가 시작됨. : %d', port);
+});
+
+server.on('connection',function(socket){
+    var addr = socket.address();
+    console.log('클라이언트가 접속 : %s, %d', addr.address, addr.port);
+});
+
+server.on('request', function(req, res){
+    console.log('클라이언트 요청이 왔습니다.');
+
+    res.writeHead(200, {"Content-Type" : "text/html; charser=utf-8"});
+
+    console.dir(req);
+});
+
+server.on('close', function(){
+    console.log('서버가 종료됩니다.');
+})
